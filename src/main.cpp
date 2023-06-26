4aaa73b9d5c7c1c1c1fabf930af5db6caae29ab3
Rune Harlyk <rune@harlyk.dk>
2023-06-26 22:22:41 +0200

    🌦 Adds weather api client

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <globals.h>
 #include <deviceconfig.h>
+#include <apiclient/weatherclient.h>
 
 ESPTaskManager g_TaskManager;
 #if USE_WIFI && USE_WEBSERVER
     DRAM_ATTR CWebServer g_WebServer;
 #endif
+
+DRAM_ATTR std::unique_ptr<WeatherApiClient> g_ptrWeatherApiClient;
+
+WiFiClientSecure client;
 
 void setup() {
   Serial.begin(115200);
@@ -15,8 +20,10 @@ void setup() {
 
   g_ptrJSONWriter = std::make_unique<JSONWriter>();
   g_ptrDeviceConfig = std::make_unique<DeviceConfig>();
+  g_ptrWeatherApiClient = std::make_unique<WeatherApiClient>(client);
 }
 
 void loop() {
-  delay(10);
+  g_ptrWeatherApiClient->getWeatherData();
+  delay(10000);
 }
